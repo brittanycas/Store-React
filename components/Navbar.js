0052d44884bfc4c8fe0a7f1fe940e67110dd9854
brittanycas <47044536+brittanycas@users.noreply.github.com>
2019-10-11 10:58:49 -0400

    Context and link corrections
    Initial Context API creation, corrected link tags in Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../Beer.png';
 
 //Need various links and formatting
@@ -7,13 +8,13 @@ export default class Navbar extends Component {
     render() {
         return (
             <div className="App-nav">
-                <a href="/">
+                <Link to={'/'}>
                     <img src={logo} className="App-logo nav-content" alt="logo" />
                     <h3 className="nav-content nav-left">Store</h3>
-                </a>
-                <a href="/cart">
+                </Link>
+                <Link to={'/cart'}>
                     <h3 className="nav-content nav-right">Cart</h3>
-                </a>
+                </Link>
             </div>
         )
     }
